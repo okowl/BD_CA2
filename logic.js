e7f1681c0dd7959e5a3c4743287e61b4732f298b
olgak <olli.kiseleva@gmail.com>
2020-04-24 17:56:12 +0100

    oh well, yet another commit

--- logic.js
+++ logic.js
@@ -1,12 +1,37 @@
+var listOfCities = [];
+
+/**
+ * This function loads twitter profile photo of selected user
+ * @param userID - Twitter user name
+ * @param twitter
+ */
 function loadPhoto(userID, twitter) {
 
     twitter.get('1.1/users/lookup.json?screen_name=' + userID).then(data => {
-        console.log(data);
-        var link = data[0].profile_image_url_https;
-        link = link.replace('_normal', '');
-        document.getElementById('photo').src = link;
+
+        var link = data[0].profile_image_url_https; //stores link to profile pic in the variable
+        link = link.replace('_normal', ''); //changes the photo size from 48x48px to original
+        document.getElementById('photo').src = link; //magic
     })
 }
+
+function loadFriendsIDs(userID, twitter){
+    twitter.get('1.1/followers/list.json?cursor=-1&screen_name=twitterdev&skip_status=true&include_user_entities=false'+ userID)
+        .then(data => {
+            var locationList = data.users;
+
+            for(var i = 0; i < locationList.length; i++) {
+
+                if (locationList[i].location !== '') {
+                    listOfCities.push(JSON.stringify(locationList[i].location));
+                }
+            }
+        })
+}
+
+/**
+ *
+ */
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -17,14 +42,15 @@ document.addEventListener("DOMContentLoaded", function () {
             // Default order: “latitude, longitude”.
             // To not manually determine the map center coordinates,
             // use the Coordinate detection tool.
-            center: [55.76, 37.64],
+            center: [53.34, 6.26], //centered on the Dublin
             // Zoom level. Acceptable values:
             // from 0 (the entire world) to 19.
-            zoom: 4
+            zoom: 2
         });
         // Auxiliary class that can be used
         // instead of GeoObject with the “Point” geometry type (see the previous example)
-        var myGeocoder = ymaps.geocode("Dublin, Ireland", {results: 1});
+
+        var myGeocoder = ymaps.geocode("Kiev, Ukraine", {results: 1});
         myGeocoder.then(
             function (res) {
                 const coords = res.geoObjects.get(0).geometry.getCoordinates();
@@ -44,10 +70,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Use popup for OAuth
     /**
-     * @TODO read documentation OAUTH and try not to shop popup if the user is logged in
+     * @TODO read documentation OAUTH and try remove popup whatsoever
      */
     OAuth.popup('twitter', {cache: true}).then(twitter => {
-        console.log('twitter:', twitter);
+
         // Prompts 'welcome' message with User's email on successful login
         // #me() is a convenient method to retrieve user data without requiring you
         // to know which OAuth provider url to call
@@ -64,16 +90,14 @@ document.addEventListener("DOMContentLoaded", function () {
         // Retrieves user data from OAuth provider by using #get() and
         // OAuth provider url
         twitter.get('/1.1/account/verify_credentials.json?include_email=true').then(data => {
-            console.log('self data:', data);
 
-        })
+        });
 
         document.getElementById('load').addEventListener('click', function () {
             var userID = document.getElementById('userID').value;
             loadPhoto(userID, twitter);
+            loadFriendsIDs(userID, twitter);
+            console.log(listOfCities);
         })
     });
-
-
-
 });
